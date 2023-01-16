f24dfef02276d73ad5ff9251ae1e1c5a8859d0e4
isamotta <isabelatlmotta@gmail.com>
2023-01-16 18:20:22 +0000

    added a message for error

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,23 +2,27 @@ const request = require('supertest');
 const app = require('../app');
 const db = require("../db/connection");
 const seed = require('../db/seeds/seed');
-const testData = require('../db/data/test-data/index')
+const testData = require('../db/data/test-data/index');
 
 beforeEach(() => {
-    return seed(testData)
+    return seed(testData);
 });
 
 afterAll(() => {
-    return db.end()
-})
+    return db.end();
+});
 
 describe("/not-a-route", () => {
-    test('responds with a 404 status code when using an API that does not exist', () => {
+    test('responds with a 404 status code when using an endpoint that does not exist', () => {
         return request(app)
             .get('/not-a-route')
-            .expect(404);
+            .expect(404)
+            .then(({ body }) => {
+                expect(body.message).toBe('does not exist');
+            })
     });
 })
+
 describe("/api/categories", () => {
     test('responds with a 200 status code and all the categories with a slug and a description property', () => {
         return request(app)
@@ -28,7 +32,7 @@ describe("/api/categories", () => {
                 expect(body.categories.length).toBe(4);
                 body.categories.forEach(category => {
                     expect(category).toHaveProperty('slug');
-                    expect(category).toHaveProperty('description')
+                    expect(category).toHaveProperty('description');
                 });
             })
     });
--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const { getAllCategories } = require('./controllers/categories')
+const { getAllCategories } = require('./controllers/categories');
 const app = express();
 
-app.get('/api/categories', getAllCategories)
+app.get('/api/categories', getAllCategories);
+app.get('*', (req, res) => {
+    res.status(404).send({ message: 'does not exist' });
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: 'internal server error' })
-})
+    res.status(500).send({ message: 'internal server error' });
+});
+
+app.listen(9090, () => {
+    console.log(`listening on 9090...`);
+});
 
 module.exports = app;
