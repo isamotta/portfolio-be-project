85f523213542f9909c0be2d96be6a516364dad40
isamotta <isabelatlmotta@gmail.com>
2023-01-16 15:19:33 +0000

    add error handle

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express();
 
 app.get('/api/categories', getAllCategories)
 
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: 'internal server error' })
+})
+
 module.exports = app;
--- controllers/categories.js
+++ controllers/categories.js
@@ -3,6 +3,8 @@ const { fetchAllCategories } = require('../models/categories')
 const getAllCategories = (req, res, next) => {
     fetchAllCategories().then((result) => {
         res.status(200).send({ categories: result })
+    }).catch((err) => {
+        next(err);
     })
 }
 
