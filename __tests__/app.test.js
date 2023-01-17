a02f326f8a2a9562b493d21487bf8de744c122b7
isamotta <isabelatlmotta@gmail.com>
2023-01-17 15:15:17 +0000

    created a get-api-reviews-id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,8 @@ const request = require('supertest');
 const app = require('../app');
 const db = require("../db/connection");
 const seed = require('../db/seeds/seed');
-const testData = require('../db/data/test-data/index');;
+const testData = require('../db/data/test-data/index'); const { expect } = require('@jest/globals');
+;
 
 beforeEach(() => {
     return seed(testData);
@@ -64,6 +65,42 @@ describe('/api/reviews', () => {
             .expect(200)
             .then(({ body }) => {
                 expect(body.reviews).toBeSortedBy('created_at', { descending: true });
+            })
+    });
+})
+
+describe('/api/reviews/:review_id', () => {
+    test('responds with a 200 status code and a review object with review_id, title, review_body, designer, review_img_url, votes, category, owner, created_at properties', () => {
+        return request(app)
+            .get('/api/reviews/1')
+            .expect(200)
+            .then(({ body }) => {
+                expect(typeof body.review[0].owner).toBe('string');
+                expect(typeof body.review[0].title).toBe('string');
+                expect(typeof body.review[0].review_id).toBe('number');
+                expect(typeof body.review[0].category).toBe('string');
+                expect(typeof body.review[0].review_img_url).toBe('string');
+                expect(typeof body.review[0].created_at).toBe('string');
+                expect(typeof body.review[0].votes).toBe('number');
+                expect(typeof body.review[0].designer).toBe('string');
+                expect(typeof body.review[0].review_body).toBe('string');
+            })
+    });
+    test('responds with a 404 status code when review_id does not exist', () => {
+        return request(app)
+            .get('/api/reviews/3000')
+            .expect(404)
+            .then(({ body }) => {
+                expect(body.message).toBe('review_id not found');
+            })
+    });
+    test('responds with a 400 status code when passed a review_id of the incorrect data type', () => {
+        return request(app)
+            .get('/api/reviews/hi')
+            .expect(400)
+            .then(({ body }) => {
+                console.log(body)
+                expect(body.message).toBe('bad request');
             })
     });
 })
