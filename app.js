72d93e5a1668f6ae0378fc37ebbd8c29cb6715c8
isamotta <isabelatlmotta@gmail.com>
2023-02-06 10:12:29 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { costumerErrorHandler, psqlErrorHandler, psqlNotFound, routeNotFound, serverErrorHandler } = require('./erros');
 const apiRouter = require('./routes/api-router');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter)
